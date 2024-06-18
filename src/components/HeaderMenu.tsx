5cc2b1d8a9d753886242b32e5b0061c0dfd0b06b
Erika Perciliano <erikaperciliano.developer@gmail.com>
2024-06-19 00:42:20 +0100

    adjusts at submenus

--- src/components/HeaderMenu.tsx
+++ src/components/HeaderMenu.tsx
@@ -1,3 +1,4 @@
+import { useRef, useState } from "react";
 import { HeaderSubMenu } from "./HeaderSubMenu";
 
 interface HeaderMenuProps {
@@ -6,11 +7,29 @@ interface HeaderMenuProps {
 }
 
 export function  HeaderMenu({ title, items }: HeaderMenuProps) {
+  const [isSubMenuOpen, setIsSubMenuOpen] = useState(false);
+  const timeoutRef = useRef<NodeJS.Timeout | null>(null);
+
+  const handleMouseEnter = () => {
+    // controla a visibilidade do submenu, limpando qualquer timeout pendente e abre o submenu imediatamente
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current);
+      timeoutRef.current = null;
+    }
+    setIsSubMenuOpen(true);
+  };
+
+  const handleMouseLeave = () => {
+    timeoutRef.current = setTimeout(() => {
+      setIsSubMenuOpen(false);
+    }, 200); // adiciona um atraso de 200ms no fechamento do submenu para evitar fechamentos acidentais. limpa o timeout se o mouse voltar a entrar antes do tempo expirar.
+  };
+  
   return (
-    <div className="group relative">
+    <div className="group relative"  onMouseEnter={handleMouseEnter} onMouseLeave={handleMouseLeave}>
     <span className="cursor-pointer">{title}</span>
-    {items && (
-      <HeaderSubMenu items={items}/>
+    {items && isSubMenuOpen && (
+      <HeaderSubMenu items={items} />
     )}
   </div>
   );
--- src/components/HeaderSubMenu.tsx
+++ src/components/HeaderSubMenu.tsx
@@ -5,11 +5,11 @@ interface HeaderSubMenuProps {
 }
 
 export function HeaderSubMenu({ items }: HeaderSubMenuProps) {
-    return (
-      <div className="absolute bg-gray-900 text-white mt-1 py-2 px-4 rounded-lg shadow-lg hidden group-hover:block">
-        {items.map((item, index) => (
-          <HeaderSubMenuItem key={index} title={item.title} />
-        ))}
+  return (
+    <div className="absolute bg-gray-900 text-white mt-1 py-2 px-4 rounded-lg shadow-lg">
+      {items.map((item, index) => (
+        <HeaderSubMenuItem key={index} title={item.title} />
+      ))}
     </div>
-    )
+  )
 }
